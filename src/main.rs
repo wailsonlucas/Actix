d814719a23ccb9a1f6d4123bcdf3a0d8b9a18704
Wailson Lucas <yassineuniversite@gmail.com>
2022-10-26 15:24:02 +0100

    custom Enums & impl for Enums

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,27 @@
 
 #[derive(Debug)]
 
-struct User {
-    username: String,
-    email: String,
-    balance: u64,
-    active: bool
+enum PaymentMethod {
+    Cash {amount: u32, name: String},
+    DebitCard { bin: u32, name: String},
 }
+
+
+impl PaymentMethod {
+    fn payment_validation(&self) -> String {
+        match *self {
+            PaymentMethod::Cash{..} => String::from("Paying with cash"),
+            PaymentMethod::DebitCard{..} => String::from("Paying with Debit Card"),
+            _ => String::from("Unsupported payment method")
+        }
+    }
+}
+
 
 fn main() {
-    let u1 = User {
-        username: String::from("Yassine"),
-        email: String::from("yassine@rust.com"),
-        balance: 1000,
-        active: true
-    };
+    let method_2:PaymentMethod = PaymentMethod::DebitCard { bin: 1000, name: String::from("Wailson") };
 
-    let u2 = User {
-        username: String::from("Wailson"),
-        email: String::from("wailson@rust.com"),
-        ..u1
-    };
-
-    println!("{:?}", u1);
-    println!("{:?}", u2);
+    let method_1:PaymentMethod = PaymentMethod::Cash{amount: 1234, name: String::from("Yassine")};
+    let checked:String = method_1.payment_validation();
+    println!("{}", checked);
 }
-
-
