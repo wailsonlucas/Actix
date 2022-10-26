6058aaae1894448c22862d2519f7089a90db0c5f
Wailson Lucas <yassineuniversite@gmail.com>
2022-10-26 08:14:52 +0100

    slice length

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let input = String::from("install");
     let x = input_validator(&input);
      match x {
-        Ok(value) => println!("installing the package with message {}", value),
+        Ok(value) => println!("installing the package with message {}", &value[2..5]),
         Err(error) => println!("the error is {}", error),
      }
 }
