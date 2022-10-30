5009c6e1a2a734cd9caa3de3c4d57017001b0548
Wailson Lucas <yassineuniversite@gmail.com>
2022-10-30 12:30:49 +0100

    custop Enum inside a Struct

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
 
-enum PaymentMethod {
-    Cash {amount: u32, name: String},
-    DebitCard { bin: u32, name: String},
+#[derive(Debug)]
+enum IP {
+    V4(String, i32),
+    V6{name: String, pl: f64}
 }
 
-
-impl PaymentMethod {
-    fn payment_validation(&self) -> String {
-        match *self {
-            PaymentMethod::Cash{..} => String::from("Paying with cash"),
-            PaymentMethod::DebitCard{..} => String::from("Paying with Debit Card"),
-            _ => String::from("Unsupported payment method")
-        }
-    }
+#[derive(Debug)]
+struct Network {
+    name: String,
+    ip: IP,
 }
 
+fn main(){
+    let web_2: Network = Network {
+        name: String::from("Web 2.0"),
+        ip: IP::V4(String::from("Web 2.0"), 3),
+    };
+    println!("{:#?}", web_2);
 
-fn main() {
-    let method_1:PaymentMethod = PaymentMethod::Cash{amount: 1234, name: String::from("Yassine")};
-    let checked:String = method_1.payment_validation();
-    println!("{}", checked);
+    let web_3:Network = Network {
+        name: String::from("Web 3.0"),
+        ip: IP::V6{name: String::from("Algeria"), pl: 12.36}
+    };
+
+    println!("{:#?}", web_3);
+
 }
+
