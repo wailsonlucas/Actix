fbffd693f88036a6afc19c6e74e8878681050249
Wailson Lucas <yassineuniversite@gmail.com>
2022-10-30 22:02:28 +0100

    implemented the math

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
-#[derive(Debug)]
-enum Vehicle {
-    Car(String)
-}
 
-impl Vehicle {
-    fn color(&self){
-        println!("red");
-    }
+#[derive(Debug)]
+struct Point {
+    x:f64,
+    y:f64
 }
     
 fn main(){
-    let car = Vehicle::Car("BMW".to_string()).color();
+
+    let a: Point = Point {
+        x: 10.00,
+        y: 12.00
+    };
+
+    let b: Point = Point {
+        x: 5.00,
+        y: 4.00
+    };
+
+    distance(a, b);
+
 }
 
+fn distance(a:Point ,b:Point) {
+    let long = a.x - b.x;
+    let width = a.y - b.y;
+    let distance = (long.powf(2.0) + width.powf(2.0)).sqrt();
+    println!("{}", distance);
+}
