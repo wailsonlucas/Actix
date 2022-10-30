cde69466895b51abc1946f9919d1b4dfa683de52
Wailson Lucas <yassineuniversite@gmail.com>
2022-10-30 13:14:48 +0100

    Tupple Struct

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,8 @@
-
 #[derive(Debug)]
-enum IP {
-    V4(String, i32),
-    V6{name: String, pl: f64}
-}
-
-#[derive(Debug)]
-struct Network {
-    name: String,
-    ip: IP,
-}
-
+struct TuppleStruct(String);
+    
 fn main(){
-    let web_2: Network = Network {
-        name: String::from("Web 2.0"),
-        ip: IP::V4(String::from("Web 2.0"), 3),
-    };
-    println!("{:#?}", web_2);
-
-    let web_3:Network = Network {
-        name: String::from("Web 3.0"),
-        ip: IP::V6{name: String::from("Algeria"), pl: 12.36}
-    };
-
-    println!("{:#?}", web_3);
-
+    let n:TuppleStruct = TuppleStruct(String::from("Mohammed"));
+    println!("{:?}", n);
 }
 
