17522f6238f056c011bd8c07dde359c779f2c9a7
Wailson Lucas <yassineuniversite@gmail.com>
2022-10-30 22:33:05 +0100

    generic struct

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,17 @@
 
+#[derive(Debug)]
+struct GenericStruct<T, U> {
+    x:T,
+    y:U
+}
+
 
 fn main(){
-    let arr:[i8; 5] = [21,12,46,42,45];
-    let x = largest(arr);
-    println!("{}", x);
-
-    let text: [char; 5] = ['a', 'b','c','d','e'];
-    let y = largest(text);
-    println!("{}", y);
+    let A:GenericStruct<i8, String> = GenericStruct {
+        x: 3,
+        y:10.to_string()
+    };
+    println!("{:#?}", A);
    
 }
 
-fn largest<T: PartialOrd + Copy>(arr: [T; 5]) -> T {
-    let mut largest = arr[0];
-    
-    for number in arr {
-        if number > largest {
-            largest = number
-        }
-    }         
-    
-    largest
-}
